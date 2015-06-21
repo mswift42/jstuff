d00d1e1b6dcbf32e9ec0b74ea277171124fdf53e
martin.haesler <martin.haesler@gmail.com>
2015-06-21 15:09:26 +0100

    Add static method print.

--- src/Diamond.java
+++ src/Diamond.java
@@ -2,4 +2,29 @@
  * Created by severin on 21/06/15.
  */
 public class Diamond {
+    public static String print(int n) {
+        StringBuffer result = new StringBuffer();
+        if (n == 1) {
+            return "*\n";
+        } else if (n % 2 == 0) {
+            return null;
+        } else {
+            int counter = 1;
+            while (counter < n) {
+                for (int i = 1; i<counter;i++) {
+                    result.append("*");
+                }
+                result.append("\n");
+                counter+=2;
+            }
+            while (counter > 1) {
+                for (int i = counter; i>1;i--) {
+                    result.append("*");
+                }
+                result.append("\n");
+                counter-=2;
+            }
+        }
+        return result.toString();
+    }
 }
