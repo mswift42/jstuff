b82166a98c7fcd6f562e3838917505cd0aacef71
martin.haesler <martin.haesler@gmail.com>
2015-06-28 19:13:19 +0100

    Implement counter class.

--- algol1/src/Counter.java
+++ algol1/src/Counter.java
@@ -1,2 +1,17 @@
 public class Counter {
+    private int count = 0;
+    private final String name;
+    Counter(String id){
+        name = id;
+    }
+    public void increment() {
+        count++;
+    }
+    public int tally() {
+        return count;
+    }
+    public String toString() {
+        return count + " " + name;
+    }
+
 }
